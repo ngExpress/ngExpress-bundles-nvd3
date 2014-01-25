969b47ee5467ea8ff6aa317c98c994331a35fdb2
Rudi Grobler <rudi@bbd.co.za>
2014-01-26 01:12:39 +0200

    major cleanup

---
+++ Gruntfile.js
@@ -0,0 +1,42 @@
+'use strict';
+
+module.exports = function(grunt) {
+    require('load-grunt-tasks')(grunt);
+    require('time-grunt')(grunt);
+
+    grunt.initConfig({
+        clean: {
+            dist: {
+                files: [{
+                    src: [
+                        'dist'
+                    ]
+                }]
+            }
+        },
+
+        concat: {
+            js: {
+                src: [
+                    'bower_components/nvd3/nv.d3.js',
+                    'bower_components/angularjs-nvd3-directives/dist/angularjs-nvd3-directives.js',
+                    'src/bundle.js'],
+                dest: 'dist/bundle.js'
+            },
+            css: {
+                src: [
+                    'bower_components/nvd3/nv.d3.css'],
+                dest: 'dist/bundle.css'
+            }
+        }
+    });
+
+    grunt.registerTask('build', [
+        'clean',
+        'concat'
+    ]);
+
+    grunt.registerTask('default', [
+        'build'
+    ]);
+};
---
+++ src/bundle.js
@@ -0,0 +1,5 @@
+'use strict';
+
+angular.module('ngExpress.bundles.nvd3', [
+    'nvd3ChartDirectives'
+]);
